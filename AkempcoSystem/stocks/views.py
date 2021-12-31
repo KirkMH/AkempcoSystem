04c7a5cb986d942bc91d638a59efd943b822a3b5
f1itss <f1itss.aklan@gmail.com>
2021-12-31 14:12:21 +0800

    Updated icons.

--- AkempcoSystem/stocks/views.py
+++ AkempcoSystem/stocks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.db.models import Q
 
 from AkempcoSystem.decorators import user_is_allowed
 from django.contrib.auth.decorators import login_required
@@ -7,6 +8,7 @@ from django.utils.decorators import method_decorator
 
 from fm.models import Product
 from admin_area.models import Feature
+from fm.views import get_index, add_search_key
 
 
 @method_decorator(login_required, name='dispatch')
@@ -15,3 +17,17 @@ class StockListView(ListView):
     model = Product
     context_object_name = "product"
     template_name = "stocks/stock_list.html"
+
+    def get_queryset(self):
+        # check if the user searched for something
+        key = get_index(self.request, "table_search")
+        object_list = self.model.objects.all()
+        if key:
+            object_list = object_list.filter(
+                Q(full_description__icontains=key)
+            )
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return add_search_key(self.request, context)    
