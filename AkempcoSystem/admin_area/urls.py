73b4b5924f2c727aac024d3235feb63cfa5a3862
Kirk M. Hilario <kirkhilario@gmail.com>
2024-02-19 23:57:47 +0800

    Change password.

--- AkempcoSystem/admin_area/urls.py
+++ AkempcoSystem/admin_area/urls.py
@@ -4,9 +4,17 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('dashboard', views.dashboard_view, name='dashboard'),
-    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(
+        template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/check', views.login_check, name='login_check'),
 
-    path('ajax/component_permissions', views.component_permissions, name='component_permissions'),
+    path('ajax/component_permissions', views.component_permissions,
+         name='component_permissions'),
+
+    path('settings/password', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html'), name='password_change'),
+    path('settings/password/done', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'), name='password_change_done'),
+
 ]
